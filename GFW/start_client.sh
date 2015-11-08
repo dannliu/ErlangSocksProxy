0ba6584790bcbb002017442e338cb7255442e1c4
Danyun Liu <danyun.liu@gmail.com>
2015-11-08 10:28:20 +0800

    move the erl to background running

--- GFW/start_client.sh
+++ GFW/start_client.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /Users/dyun/Program/GitHub/ERLSocks/GFW
-erlc /Users/dyun/Program/GitHub/ERLSocks/GFW/gfw_client.erl
+cd /Users/dyun/Program/GitHub/dyun/ERLSocks/GFW
+erlc /Users/dyun/Program/GitHub/dyun/ERLSocks/GFW/gfw_client.erl
 erl -noshell -s gfw_client start -detached
--- GFW/start_server.sh
+++ GFW/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 erlc gfw_socks.erl
-erl -noshell -s gfw_socks start
+erl -noshell -s gfw_socks start -detached > log.txt
